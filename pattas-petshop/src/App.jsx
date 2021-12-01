2386c42c02b2973188f63b61667245518b229b32
Destinycod <clara.rodriguez.a@hotmail.com>
2021-11-30 22:30:49 -0300

    Actualizacion

--- pattas-petshop/src/App.jsx
+++ pattas-petshop/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -38,6 +39,7 @@ function App() {
           </a>
         </p>
       </header>
+      <NavBar/>
     </div>
   )
 }
