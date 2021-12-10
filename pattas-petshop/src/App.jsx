08d28ac56216c5869d677e7d37300fab6a944c77
Destinycod <clara.rodriguez.a@hotmail.com>
2021-12-09 21:31:53 -0300

    Actividad 6-Promises

--- pattas-petshop/src/App.jsx
+++ pattas-petshop/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer'
 import ItemCount from './components/ItemCount/ItemCount'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
@@ -11,11 +13,18 @@ function App() {
 
   return (
     <div className="App">
+      {/*<BrowserRouter>
         <header className="App-header">
             <NavBar/>  
         </header>
-        <ItemListContainer greetings = "soy el greeting" />
+        <Routes>
+          <Route exact path="/"
+          element { <ItemListContainer greetings = "soy el greeting" /> } />
+        </Routes>
         <ItemCount minimo={minimo} stock={stock}/>
+      <BrowserRouter/>*/}
+      <NavBar />
+      <ItemListContainer greetings = "soy el greeting" />
     </div>
   )
 }
--- pattas-petshop/src/components/ItemListContainer.jsx
+++ pattas-petshop/src/components/ItemListContainer.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { getFetch } from '../helpers/getFetch';
 
 function ItemListContainer( {greetings} ){
+
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    //const { idCate } = useParams() 
+    
+    
+    useEffect(() => {
+        /*if (idCate) {
+            getFetch
+            .then(resp => setProductos(resp.filter(prod => prod.categoria === idCate))) 
+            .catch(err => console.log(err))
+            .finally(()=>setLoading(false))            
+        } else {*/
+            getFetch
+            .then(resp => setProductos(resp)) 
+            .catch(err => console.log(err))
+            .finally(()=>setLoading(false))               
+        
+    })  
+    
+    //console.log(idCate)
+
 
     return (
         <div>
             {greetings}
+            { loading ? 
+                <h2>Cargando...</h2> 
+              :  
+               <ItemList productos={productos} />
+            }
+
         </div>
     );
 }
